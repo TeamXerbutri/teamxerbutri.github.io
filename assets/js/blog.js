6ad6cc131a86686a9dda0863101c706ddfd67147
Helmer <helmer.dendekker@beterbed.net>
2024-03-05 09:37:05 +0100

    Creating the aside
    Started with the aside

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -10,6 +10,16 @@ uiState.hasPrivacyModal = false;
 uiState.hasShareModal = false;
 uiState.hasBackToTop = false;
 
+function countProperties(obj) {
+	let count = 0;
+
+	for(const prop in obj) {
+		if(obj.hasOwnProperty(prop))
+			++count;
+	}
+
+	return count;
+}
 
 //TODO need to translate FFF to JS
 // 	<script src="../picz/{{@categorie}}/{{@map}}/items.js"></script>
@@ -88,6 +98,11 @@ export function initBlog() {
 	document.querySelector('#app').innerHTML = `
 		<article id="blog">
 		<div id="article-title"></div>
+		<p id="article-created" class="authordate"></p>
+		<p id="article-intro"></p>
+		<aside id="article-aside"></aside>
+		<section id="article-content"></section>
+		<p id="article-updated" class="authordate"></p>
 		</article>
 		<a id="back-to-top" href="#blog">^</a>
 		`
@@ -129,21 +144,241 @@ export function initBlog() {
 			document.title = value.shortname + " - Xerbutri Urban Exploring";
 			document.querySelector('meta[name="description"]').setAttribute("content", value.description);
 			document.getElementById("article-title").innerHTML = `<h1>${value.shortname}</h1>`;
+			
 			const getBlogLanguageContent = fetch("../../data/".concat(value.category,"/",abbreviation,"/blog.", appState.language, ".json")).then((response) => response.json());
-			const blogContent =  getBlogLanguageContent.then(
-				function (value) {
-					return value;
+			getBlogLanguageContent.then(
+				function (blogContent) {
+					document.getElementById("article-intro").innerHTML = blogContent.intro;
 				},
 				errorHandler
 			);
+			
 			const getBlogFacts = fetch("../../data/".concat(value.category,"/",abbreviation,"/blog.json")).then((response) => response.json());
-			const blogFacts =  getBlogFacts.then(
-				function (value) {
-					return value;
+			
+			getBlogFacts.then(
+				function (blogFacts) {
+					
+					const year = blogFacts["created"].split("-")[0];
+					const month = blogFacts["created"].split("-")[1];
+					let monthBlog = "";
+					if(appState.language === "nl"){
+						switch (month) {
+							case "01":
+								monthBlog = "januari";
+								break;
+							case "02":
+								monthBlog = "februari";
+								break;
+							case "03":
+								monthBlog = "maart";
+								break;
+							case "04":
+								monthBlog = "april";
+								break;
+							case "05":
+								monthBlog = "mei";
+								break;
+							case "06":
+								monthBlog = "juni";
+								break;
+							case "07":
+								monthBlog = "juli";
+								break;
+							case "08":
+								monthBlog = "augustus";
+								break;
+							case "09":
+								monthBlog = "september";
+								break;
+							case "10":
+								monthBlog = "oktober";
+								break;
+							case "11":
+								monthBlog = "november";
+								break;
+							case "12":
+								monthBlog = "december";
+								break;
+						}
+					}
+					//ToDo English/French/Deutsch
+					
+					document.getElementById("article-created").innerHTML = `${blogFacts.author} -  ${monthBlog} ${year}`;
+					
+					//aside
+					if(countProperties(blogFacts.facts)>0){
+						document.getElementById("article-aside").innerHTML += `<ul>`;
+						Object.entries(blogFacts.facts).forEach(([key, value]) => {
+							if(key==="Rating"){
+								document.getElementById("article-aside").innerHTML += `<li>${key}: <span class="fact"><img src="../ui/pics/ri${value}.gif" alt="${value}" width="152" height="10" /></span></li>`;
+							}
+							
+							if(key==="Build"){
+								let buildKey = "";
+								switch (appState.language) {
+									case "nl":
+										buildKey = "Bouwjaar";
+										break;
+									case "en":
+										buildKey = "Built";
+										break;
+									case "fr":
+										buildKey = "Construit";
+										break;
+									case "de":
+										buildKey = "Gebaut";
+										break;
+								}
+								document.getElementById("article-aside").innerHTML += `<li>${buildKey}: <span class="fact">${value}</span> </li>`;
+							}
+
+							if(key==="Abandoned"){
+								let abandonedKey = "";
+								switch (appState.language) {
+									case "nl":
+										abandonedKey = "Verlaten";
+										break;
+									case "en":
+										abandonedKey = "Abandoned";
+										break;
+									case "fr":
+										abandonedKey = "Abandonné";
+										break;
+									case "de":
+										abandonedKey = "Verlassen";
+										break;
+								}
+								document.getElementById("article-aside").innerHTML += `<li>${abandonedKey}: <span class="fact">${value}</span> </li>`;
+							}
+
+							if(key==="Visited"){
+								let visitedKey = "";
+								switch (appState.language) {
+									case "nl":
+										visitedKey = "Bezocht";
+										break;
+									case "en":
+										visitedKey = "Visited";
+										break;
+									case "fr":
+										visitedKey = "Visitée";
+										break;
+									case "de":
+										visitedKey = "Besucht";
+										break;
+								}
+								document.getElementById("article-aside").innerHTML += `<li>${visitedKey}: <span class="fact">${value}</span> </li>`;
+							}
+
+							if(key==="Demolished"){
+								let demolishKey = "";
+								switch (appState.language) {
+									case "nl":
+										demolishKey = "Gesloopt";
+										break;
+									case "en":
+										demolishKey = "Demolished";
+										break;
+									case "fr":
+										demolishKey = "Démoli";
+										break;
+									case "de":
+										demolishKey = "Abgerissen";
+										break;
+								}
+								document.getElementById("article-aside").innerHTML += `<li>${demolishKey}: <span class="fact">${value}</span> </li>`;
+							}
+
+							if(key==="Reused"){
+								let reuseKey = "";
+								switch (appState.language) {
+									case "nl":
+										reuseKey = "Hergebruikt";
+										break;
+									case "en":
+										reuseKey = "Reused";
+										break;
+									case "fr":
+										reuseKey = "Réutilisé";
+										break;
+									case "de":
+										reuseKey = "Wiederverwendet";
+										break;
+								}
+								document.getElementById("article-aside").innerHTML += `<li>${reuseKey}: <span class="fact">${value}</span> </li>`;
+							}
+							
+							if(key==="Length"){
+								let lengthKey = "";
+								switch (appState.language) {
+									case "nl":
+										lengthKey = "Lengte";
+										break;
+									case "en":
+										lengthKey = "Length";
+										break;
+									case "fr":
+										lengthKey = "Longueur";
+										break;
+									case "de":
+										lengthKey = "Länge";
+										break;
+								}
+								document.getElementById("article-aside").innerHTML += `<li>${lengthKey}: <span class="fact">${value}</span> </li>`;
+							}
+
+							if(key==="Line"){
+								let lineKey = "";
+								switch (appState.language) {
+									case "nl":
+										lineKey = "Lijn nummer";
+										break;
+									case "en":
+										lineKey = "Line number";
+										break;
+									case "fr":
+										lineKey = "Ligne";
+										break;
+									case "de":
+										lineKey = "Streckennummer";
+										break;
+								}
+								document.getElementById("article-aside").innerHTML += `<li>${lineKey}: <span class="fact">${value}</span> </li>`;
+							}
+							if (key==="Map") {
+								let mapKey = "";
+								switch (appState.language) {
+									case "nl":
+										mapKey = "Overzichtskaart";
+										break;
+									case "en":
+										mapKey = "Map";
+										break;
+									case "fr":
+										mapKey = "Carte";
+										break;
+									case "de":
+										mapKey = "Karte";
+										break;
+								}
+								document.getElementById("article-aside").innerHTML += `<li>${mapKey} </br><div class="omap" id="omap" data-map="${value}"></div> </li>`;
+							}
+						});
+						document.getElementById("article-aside").innerHTML += `</ul>`;
+					}
+					
+					
+					
 				},
 				errorHandler
 			);
-			// ToDo construct the blog content from tge language content and facts in a blogfactory
+			// ToDo construct the blog content from the language content and facts in a blogfactory
+			
+			
+			
+			
+			
+			
 		}, errorHandler
 	);
 	
