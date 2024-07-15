a5461c92c0ba64072c8065e55bd468dbe00b564d
Helmer <helmer.dendekker@beterbed.net>
2024-07-15 16:51:05 +0200

    resizing fix

--- assets/js/map.js
+++ assets/js/map.js
@@ -22,6 +22,12 @@ export function initMap() {
 	let vh = window.innerHeight * 0.01;
 	// Then we set the value in the --vh custom property to the root of the document
 	document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+	window.addEventListener('resize', () => {
+		// We execute the same script as before
+		let vh = window.innerHeight * 0.01;
+		document.documentElement.style.setProperty('--vh', `${vh}px`);
+	});
 	
 	// do not show header in map
 	const headerElem = document.getElementById("header");
