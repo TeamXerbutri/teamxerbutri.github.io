b067ce6895b8dc9ece71a01b6d380873ac7c9b5d
Helmer <helmer.dendekker@beterbed.net>
2025-04-16 13:35:20 +0200

    code style changes

--- assets/js/pathrouter.js
+++ assets/js/pathrouter.js
@@ -28,40 +28,50 @@ let stateContext = function () {
 
 	this.initState = function () {
 		const path = window.location.pathname.toLowerCase();
+		
 		if (path.startsWith("/cms")) {
 			this.transitionTo(new cmsState(this.context));
 			return;
 		}
+		
 		if (path.startsWith("/map")) {
 			this.transitionTo(new mapState(this.context));
 			return;
 		}
+		
 		if (path.startsWith("/avontuur")) {
 			this.transitionTo(new blogState(this.context));
 			return;
 		}
+		
 		this.transitionTo(new homeState(this));
 	}
 }
 
 let homeState = function (context) {
+	
 	this.context = context;
+	
 	this.enterState = function () {
 		initHome();
 	}
+	
 	this.navigate = function (path) {
-		console.log("home navigating to " + path);
+		
 		if (path.length === 0 || path.startsWith("/vijf")) {
 			// do nothing, I am already @home
 			return;
 		}
+		
 		if (path.startsWith("/avontuur")) {
 			this.context.transitionTo(new blogState(this.context));
 			return;
 		}
+		
 		if (path.startsWith("/map")) {
 			this.context.transitionTo(new mapState(this.context));
 		}
+		
 		if (path.startsWith("/cms")) {
 			this.context.transitionTo(new cmsState(this.context));
 		}
@@ -69,23 +79,29 @@ let homeState = function (context) {
 }
 
 let blogState = function (context) {
+	
 	this.context = context;
+	
 	this.enterState = function () {
-		console.log("init blog");
 		initBlog();
 	}
+	
 	this.navigate = function (path) {
+		
 		if (path.length === 0 || path.startsWith("/vijf")) {
 			this.context.transitionTo(new homeState(this.context));
 			return;
 		}
+		
 		if (path.startsWith("/map")) {
 			this.context.transitionTo(new mapState(this.context));
 			return;
 		}
+		
 		if (path.startsWith("/avontuur")) {
 			this.context.transitionTo(new blogState(this.context));
 		}
+		
 		if (path.startsWith("/cms")) {
 			this.context.transitionTo(new cmsState(this.context));
 		}
@@ -102,13 +118,16 @@ let mapState = function (context) {
 			this.context.transitionTo(new homeState(this.context));
 			return;
 		}
+		
 		if (path.startsWith("/map")) {
 			// do nothing, I am already @map
 			return;
 		}
+		
 		if (path.startsWith("/avontuur")) {
 			this.context.transitionTo(new blogState(this.context));
 		}
+		
 		if (path.startsWith("/cms")) {
 			this.context.transitionTo(new cmsState(this.context));
 		}
@@ -117,21 +136,26 @@ let mapState = function (context) {
 
 let cmsState = function (context) {
 	this.context = context;
+	
 	this.enterState = function () {
 		initCms();
 	}
+	
 	this.navigate = function (path) {
 		if (path.length === 0 || path.startsWith("/vijf")) {
 			this.context.transitionTo(new homeState(this.context));
 			return;
 		}
+		
 		if (path.startsWith("/map")) {
 			this.context.transitionTo(new mapState(this.context));
 			return;
 		}
+		
 		if (path.startsWith("/avontuur")) {
 			this.context.transitionTo(new blogState(this.context));
 		}
+		
 		if (path.startsWith("/cms")) {
 			// do nothing, I am already @cms
 		}
