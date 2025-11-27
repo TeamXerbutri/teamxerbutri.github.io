5474a5fc8ebb76c6c46188dce83da1612b1e1c5c
Helmer <helmer.dendekker@beterbed.net>
2025-11-27 08:41:52 +0100

    Move to .env files

--- assets/config.js
+++ assets/config.js
@@ -1,5 +1,10 @@
-const ApiBasePath = "http://localhost:5174/api";
-const ImageBasePath = "http://localhost:5174/api";
+// Use Vite environment variables when available, otherwise fall back to localhost defaults
+const ApiBasePath = import.meta && import.meta.env && import.meta.env.VITE_API_BASE_PATH
+	? import.meta.env.VITE_API_BASE_PATH
+	: "http://localhost:5174/api";
+const ImageBasePath = import.meta && import.meta.env && import.meta.env.VITE_IMAGE_BASE_PATH
+	? import.meta.env.VITE_IMAGE_BASE_PATH
+	: ApiBasePath;
 export const versionBasePath = () =>{
 	const basePath = window.location.origin ? window.location.origin + '/' : window.location.protocol + '/' + window.location.host + '/';
 	return basePath + "data";
