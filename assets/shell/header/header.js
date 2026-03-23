a208a59535b680a51c09f28e40a4d856ac2f4eea
Ferdinand Xerbutri <xerbutri@proton.me>
2026-03-23 09:19:03 +0100

    update to 48px icons

--- assets/shell/header/header.js
+++ assets/shell/header/header.js
@@ -1,10 +1,12 @@
 import {leftArrow} from "../../shared/icons/icons.js";
 
-const dotsMenu = '<svg aria-hidden="true" class="icon_dark" viewBox="0 0 24 24" width="24" height="24"><circle r="3" cx="12" cy="3"/><circle r="3" cx="12" cy="12"/><circle r="3" cx="12" cy="21"/></svg>';
+const dotsMenu = '<svg aria-hidden="true" class="icon_dark" viewBox="0 0 48 48" width="48" height="48"><circle r="6" cx="24" cy="6"/><circle r="6" cx="24" cy="24"/><circle r="6" cx="24" cy="42"/></svg>';
 
 const share = '<svg aria-hidden="true" class="icon_dark" viewBox="0 0 24 24" width="24" height="24"><circle r="4" cx="4" cy="12"/><circle r="4" cx="20" cy="20"/><circle r="4" cx="20" cy="4"/><path d="M22,22 L22,22 L2,12 L22,2 L22,4 L6,12 L22,20 Z"/></svg>';
 
 const txLogo = '<svg aria-hidden="true" class="icon_dark" viewBox="0 0 24 24" width="24" height="24"><path d="M0,0 L24,0 L24,7 L22,4 L14.5,4 L14.5,22 L16,24 L14,24 L17.7,18.9 L21,24 L24,24 L19.3,16.7 L24,10 L22,10 L18.3,15.2 L15,10 L12,10 L16.7,17.4 L12,24 L8,24 L9.5,22 L9.5,4 L2,4 L0,6 Z" /></svg>';
+
+const searchIcon = '<svg aria-hidden="true" class="icon_dark" viewBox="0 0 24 24" width="24" height="24"><circle r="4" cx="4" cy="12"/><circle r="4" cx="20" cy="20"/><circle r="4" cx="20" cy="4"/><path d="M22,22 L22,22 L2,12 L22,2 L22,4 L6,12 L22,20 Z"/></svg>';
 
 const privacyHtml = `<div id="privacy-panel" class="panel hide">
 			<h2 data-i18n="privacy.link">Privacy</h2>
